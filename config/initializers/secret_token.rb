5abf749c147a6ffac1ca0e7f8433cd2eb8c15a0f
Girts Silis <girts.silis@gmail.com>
2014-04-29 11:34:00 -0400

    Initial commit of blank rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AndroidPhotobucketServer::Application.config.secret_key_base = '225068a22714d084786142870a7d69c4972c60cf58c0f4c06eea8eb23c4663a8296143a36ffa37168d0b7f26f14169aa4c4e2c04b2413dd4e15c8a0cceaa62e5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AndroidPhotobucketServer::Application.config.session_store :cookie_store, key: '_android-photobucket-server_session'
