bb76b7cf31ff0c572b3750e0018d44c34d0c7a87
Girts Silis <girts.silis@gmail.com>
2014-04-29 13:11:08 -0400

    ApiKey model defined

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,2 +1,13 @@
 class ApiKey < ActiveRecord::Base
+  before_create :generate_token
+
+  validates_presence_of   :token, unless: :new_record?
+  validates_uniqueness_of :token
+
+  private
+    def generate_token
+      begin
+        self.token = SecureRandom.hex
+      end while self.class.exists?( token: self.token )
+    end
 end
