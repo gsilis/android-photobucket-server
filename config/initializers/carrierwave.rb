8e6412444963daf6168bc6f558f541eb6c1cdfa8
Girts Silis <girts.silis@gmail.com>
2014-04-30 12:00:01 -0400

    Carrierwave configuration for fog

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,20 @@
+CarrierWave.configure do |config|
+  if Rails.env.production?
+
+    # Fog setup for comm with S3
+    config.storage = :fog
+    config.fog_credentials do |config|
+      provider:               'AWS',
+      aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
+    end
+    config.fog_directory      = 'android-dev-photobucket'
+    config.fog_public         = false
+
+  else
+
+    # Don't upload to anything for non-prod
+    config.storage = :file
+
+  end
+end
