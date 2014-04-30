b5b5a79661b79e25821f03fbbc4c5b96829bb810
Girts Silis <girts.silis@gmail.com>
2014-04-30 12:20:54 -0400

    Carrierwave bug fix -- oops

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,11 +3,12 @@ CarrierWave.configure do |config|
 
     # Fog setup for comm with S3
     config.storage = :fog
-    config.fog_credentials do |config|
+    config.fog_credentials = {
       provider:               'AWS',
       aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
-    end
+      aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY']
+    }
+
     config.fog_directory      = 'android-dev-photobucket'
     config.fog_public         = false
 
